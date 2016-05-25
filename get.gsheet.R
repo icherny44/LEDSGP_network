7daa621336c820fa6c4bdbbf87301ad57489b632
icherny44 <cherny.ilya@gmail.com>
2016-05-25 09:09:17 -0600

    Function to pull data from google sheets

--- get.gsheet.R
+++ get.gsheet.R
@@ -7,9 +7,10 @@ library(googlesheets)
 library(RCurl)
 library(httr)
 
+set_config( config( ssl_verifypeer = FALSE ) )
+
 # Fetch sheet registration from G-docs
 if(!exists("gap_gsheet")){
-  set_config( config( ssl_verifypeer = FALSE ) )
   sheet_url <- "https://docs.google.com/spreadsheets/d/1jKtc49RjMg9AtnSF89nAoGFzTKjGU628WrHfxH21dAs/edit?usp=sharing"
   gap_gsheet <- gs_url(sheet_url)
 }
